83e80f0f4ab22f4427cb77e9ff954b4f072a537f
omarnassar-school <377576@bsd48.org>
2019-09-23 08:56:25 -0700

    first commit

---
+++ .bash_history
@@ -0,0 +1,25 @@
+ls
+cd
+cd ..
+ls
+cd 460712
+ls
+cd ..
+cd 347814
+ls
+cd projects
+ls
+cd projects
+ls
+cd ..
+ cd ..
+ls
+cd 370801
+ls
+cd projects
+ls
+cd ..
+ cd ..
+cd 377576
+ls
+exit
--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -5,54 +5,63 @@ using namespace std;
 
 bool checkWin(int);
 bool checkTie();
-void printBoard();
+void printBoard(char* Prnt[3][3]);
+void clearBoard(char* Prnt[3][3]);
 
 int main() {
-  
-  int board[3][3];
-  const int BLANK = 0;
-  const int X_MOVE = 1;
-  const int O_MOVE = 2;
-  const int X_TURN = 0;
-  const int O_TURN = 1;
-  int turn = X_TURN;
-  int X_wins = 0;
-  int O_wins = 0;
+  char board[3][3];
+  char* boardPrnt[3][3];
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      boardPrnt[i][j] = &board[i][j];
+    }
+  }
+  clearBoard(boardPrnt);
+  char input[2];
+  char player = 'O';
 
   bool stillPlaying = true;
-  while (stillPlaying == true) { //&& checkWin(X_MOVE) == false && checkWin(O_MOVE) == false && checkTie() == false) {
-    printBoard();
+  while (stillPlaying == true) {
+    printBoard(boardPrnt);
+    if (player == 'X') {
+      player = 'O';
+    }
+    else {
+      player = 'X';
+    }
+    cin.get(input, 3);
+    cin.clear();
+    cin.ignore(1000000, '\n');
+    if ((input[0] == 'a' || input[0] == 'b' || input[0] == 'c')
+	&& (input[1] == '1' || input [1] == '2' || input[1] == '3')) {
+      board[(int) input[0] - 'a'][(int) input[1] - '1'] = player;
+    }
+    //stillPlaying = false;
   }
+  printBoard(boardPrnt);
   
   return 0;
 }
 
 bool checkWin(int player) {
-  if (player == 0) {
-    return false;
-  }
-  else {
-    return true;
-  }
+  
 }
 
 bool checkTie() {
 
 }
 
-void printBoard() {
-  cout << " \t1\t2\t3" << endl;
-  for (int row = 0; row < 3; row++) {
-    char output = ('a' + row) + "\t";
-    for (int column = 0; column < 3; column++) {
-      //if (board[row][column] == BLANK) {
-	output += "\t";
-	//}
-	//else if (board[row][column] == X_MOVE) {
-	//output += "X\t";
-	//      }
-	//      else if (board[row][column] == O_MOVE) {
-	//	output += "0\t";
-	//      }
+void printBoard(char* Prnt[3][3]) {
+  cout << "  1 2 3" << endl;
+  cout << "a " << *Prnt[0][0] << " " << *Prnt[0][1] << " " << *Prnt[0][2] << endl;
+  cout << "b " << *Prnt[1][0] << " " << *Prnt[1][1] << " " << *Prnt[1][2] << endl;
+  cout << "c " << *Prnt[2][0] << " " << *Prnt[2][1] << " " << *Prnt[2][2] << endl;
+}
+
+void clearBoard(char* Prnt[3][3]) {
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 3; j++) {
+      *Prnt[i][j] = ' ';
     }
-    cout << output << endl;
+  }
+}
