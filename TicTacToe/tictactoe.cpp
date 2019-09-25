a973dbed2261b5b71208fc9ea6d7391b016582ff
omarnassar-school <377576@bsd48.org>
2019-09-25 09:13:34 -0700

    first commit

--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -1,16 +1,20 @@
-
+/*Author: Omar Nassar
+ *Date: 9/25/19
+ *This is a 2 player Tic Tac Toe game in the terminal
+ */
 
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
-bool checkWin(int);
-bool checkTie();
+bool checkWin(char* Prnt[3][3], char player);
+bool checkTie(char* Prnt[3][3]);
 void printBoard(char* Prnt[3][3]);
 void clearBoard(char* Prnt[3][3]);
 
 int main() {
+  cout << "Welcome to Tic Tac Toe. Enter a row followed by a column to choose your spot." << endl;
   char board[3][3];
   char* boardPrnt[3][3];
   for (int i = 0; i < 3; i++) {
@@ -21,36 +25,112 @@ int main() {
   clearBoard(boardPrnt);
   char input[2];
   char player = 'O';
-
+  int x_wins = 0;
+  int o_wins = 0;
   bool stillPlaying = true;
-  while (stillPlaying == true) {
+  bool game = true;
+  while (game == true) {
     printBoard(boardPrnt);
-    if (player == 'X') {
-      player = 'O';
+    while (stillPlaying == true) {
+      if (player == 'X') {
+	player = 'O';
+      }
+      else {
+	player = 'X';
+      }
+      cin.get(input, 3);
+      cin.clear();
+      cin.ignore(1000000, '\n');
+      if ((input[0] == 'a' || input[0] == 'b' || input[0] == 'c')
+	  && (input[1] == '1' || input[1] == '2' || input[1] == '3')
+	  && (board[(int) input[0] - 'a'][(int) input[1] - '1'] == ' ')) {
+	board[(int) input[0] - 'a'][(int) input[1] - '1'] = player;
+      }
+      else {
+	cout << "Invalid output. Try again." << endl;
+	if (player == 'X') {
+	  player = 'O';
+	}
+	else {
+	  player = 'X';
+	}
+      }
+      printBoard(boardPrnt);
+      if (checkWin(boardPrnt, player) == true) {
+	cout << player << " wins!" << endl;
+	if (player == 'X') {
+	  x_wins++;
+	}
+	else {
+	  o_wins++;
+	}
+	stillPlaying = false;
+	cout << "X wins: " << x_wins << endl;
+	cout << "O wins: " << o_wins << endl;
+      }
+      else if (checkTie(boardPrnt) == true) {
+	cout << "Tie Game." << endl;
+	stillPlaying = false;
+      }
     }
-    else {
-      player = 'X';
-    }
-    cin.get(input, 3);
+    cout << "Would you like to play again? (y/n)" << endl;
+    char answer;
+    cin >> answer;
     cin.clear();
     cin.ignore(1000000, '\n');
-    if ((input[0] == 'a' || input[0] == 'b' || input[0] == 'c')
-	&& (input[1] == '1' || input[1] == '2' || input[1] == '3')) {
-      board[(int) input[0] - 'a'][(int) input[1] - '1'] = player;
+    if (answer == 'y') {
+      clearBoard(boardPrnt);
+      stillPlaying = true;
+      player = 'O';
     }
-    //stillPlaying = false;
+    else if (answer == 'n') {
+      cout << "Thanks for playing!" << endl;
+      break;
+    }
+    else {
+      cout << "Invalid answer. Try again." << endl;
+    }
   }
-  printBoard(boardPrnt);
-  
   return 0;
 }
 
-bool checkWin(int player) {
-  
+bool checkWin(char* Prnt[3][3], char player) {
+  if (*Prnt[0][0] == player && *Prnt[0][1] == player && *Prnt[0][2] == player) {//top row
+    return true;
+  }
+  if (*Prnt[0][0] == player && *Prnt [1][1] == player && *Prnt [2][2] == player) {//top left to bottom right diagonal
+    return true;
+  }
+  if (*Prnt[1][0] == player && *Prnt[1][1] == player && *Prnt[1][2] == player) {//middle row
+    return true;
+  }
+  if (*Prnt[2][0] == player && *Prnt[2][1] == player && *Prnt[2][2] == player) {//bottom row
+    return true;
+  }
+  if (*Prnt[0][0] == player && *Prnt[1][0] == player && *Prnt[2][0] == player) {//left column
+    return true;
+  }
+  if (*Prnt[0][1] == player && *Prnt[1][1] == player && *Prnt[2][1] == player) {//middle column
+    return true;
+  }
+  if (*Prnt[0][2] == player && *Prnt[1][2] == player && *Prnt[2][2] == player) {//right column
+    return true;
+  }
+  if (*Prnt[0][2] == player && *Prnt [1][1] == player && *Prnt [2][0] == player) {//top right to bottom left diagonal
+    return true;
+  }
+  return false;
 }
 
-bool checkTie() {
-
+bool checkTie(char* Prnt[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (*Prnt[i][j] == ' ') {
+	return false;
+      }
+    }
+  }
+  return true;
 }
 
 void printBoard(char* Prnt[3][3]) {
