534f1581f392d37b7792b00d2e4dba420b31c6ea
omarnassar-school <377576@bsd48.org>
2019-09-27 08:07:06 -0700

    first commit

---
+++ StudentList/student.cpp
@@ -0,0 +1,16 @@
+/*Author: Omar Nassar
+ *Date: 9/27/19
+ *This a program that can store a list of students first and last names, id, and GPA and print
+ *them out or delete them
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  
+  
+  return 0;
+}
