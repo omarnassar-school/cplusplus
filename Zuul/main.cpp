3eaa1d0166ff4ba4d44fe7632e6d30f6fb9d9642
omarnassar-school <377576@bsd48.org>
2019-10-16 08:26:38 -0700

    first commit

---
+++ Zuul/main.cpp
@@ -0,0 +1,20 @@
+/* Author: Omar Nassar
+ * Date: 10/16/19
+ * This is an adventure game based off of the "World of Zuul" application by Michael Kolling and David J. Barnes
+ */
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  
+  cout << "You are late to school and are rushing to get to school when someone runs up from behind you and takes your things. You try to catch him but he is too fast. He drops your backpack but keeps running with a few of your things. He runs inside the school and hides somewhere. What you need to find along with your backpack is your pencil, eraser, late slip, calculator, and your graded test that your parents signed and you were supposed to return today." << endl;
+
+  cout << endl << "Type 'help' if you need help." << endl;
+  
+  
+  return 0;
+}
