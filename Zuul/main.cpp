0b9336c8485236ae3f4b0f207045642e9f4388d3
U-BSD\377576 <377576@SHS-10DEC49DF.beaverton.k12.or.us>
2019-10-22 09:04:58 -0700

    first commit

--- Zuul/main.cpp
+++ Zuul/main.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <cstring>
+#include <iomanip>
 #include <vector>
 #include <map>
 #include "room.h"
@@ -16,11 +17,13 @@ struct Item {
   int id;
 };
 
+//void initializeRooms(vector<Room*>* rooms);
+//void initializeItems(vector<Item*>* items);
+
 int main() {
-  vector<Room*> room;
+  vector<Room*> rooms;
   vector<Item*> items;
   char input[10];
-  
   cout << "You are late to school and are rushing to get to school when someone runs up from behind you and takes your things. You try to catch him but he is too fast. He drops your backpack but keeps running with a few of your things. He runs inside the school and hides somewhere. What you need to find along with your backpack is your pencil, eraser, late slip, calculator, and your graded test that your parents signed and you were supposed to return today." << endl;
 
   cout << endl << "Type 'help' if you need help." << endl << endl;
@@ -44,3 +47,11 @@ int main() {
   }
   return 0;
 }
+
+//void initializeRooms(vector<Room*>* rooms) {
+//  
+//}
+
+//void initializeItems(vector<Items*>* items) {
+//  
+//}
