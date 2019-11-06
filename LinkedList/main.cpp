f363ddb257319ef5cdba499b85f67ed25e6ef971
U-BSD\377576 <377576@SHS-10DEC49DF.beaverton.k12.or.us>
2019-11-06 08:51:07 -0800

    first commit

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -34,7 +34,32 @@ int main() {
   printf("%.2f\n", *student -> getGPA());
   cout << endl;
   
-  
+  Student* student2 = new Student;
+  cout << "Enter a first name: ";
+  cin.get(student2 -> getFirst(), 10);
+  cin.clear();
+  cin.ignore(1000000, '\n');
+  //student2 -> setFirst((char*)(first));
+  cout << "Enter a last name: ";
+  cin.get(student2 -> getLast(), 10);
+  cin.clear();
+  cin.ignore(1000000, '\n');
+  //student2 -> setLast((char*)(last));
+  cout << "Enter an ID number: ";
+  cin >> *student2 -> getID();
+  cin.clear();
+  cin.ignore(1000000, '\n');
+  //student2 -> setID(ID);
+  cout << "Enter a GPA: ";
+  cin >> *student2 -> getGPA();
+  cin.clear();
+  cin.ignore(1000000, '\n');
+  //student2 -> setGPA(GPA);
+  cout << endl;
+  cout << student -> getFirst() << " " << student2 -> getLast() << endl;
+  cout << *student -> getID() << endl;
+  printf("%.2f\n", *student2 -> getGPA());
+  cout << endl;
   
   Node* firstNode = new Node;
   Node* secondNode = new Node;
@@ -43,7 +68,6 @@ int main() {
   secondNode -> setStudent(student2);
   
   cout << firstNode -> getStudent() -> getFirst() << firstNode -> getStudent() -> getLast() << endl;
-  //cout << firstNode -> getNext() -> *getStudent() -> getFirst() << firstNode -> getNext() -> *getStudent -> getLast() << endl;
-  */
+  cout << firstNode -> getNext() -> getStudent() -> getFirst() << firstNode -> getNext() -> getStudent() -> getLast() << endl;
   return 0;
 }
