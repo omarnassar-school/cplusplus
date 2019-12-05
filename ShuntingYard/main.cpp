e3373fa4ae698b8234e10e3265d5dc0612475972
U-BSD\377576 <377576@SHS-10DEC49DF.beaverton.k12.or.us>
2019-12-05 08:48:48 -0800

    first commit

---
+++ ShuntingYard/main.cpp
@@ -0,0 +1,45 @@
+/* Author: Omar Nassar
+ * Date: 12/5/2019
+ * This is a program that can translate infix to postfix and create an expression tree.
+ * This can then be outputted as either infix, prefix, or postfix
+ */
+
+#include <iostream>
+#include <cstring>
+#include "node.h"
+
+
+using namespace std;
+
+int main() {
+  char input[100];
+  cout << "Enter an infix expression" << endl << endl;
+  cin.get(input, 100);
+  cin.clear();
+  cin.ignore(1000000, '\n');
+
+  char method[7];
+  while (true) {
+    cout << endl << "What way would you like to output this expression? (Infix, Prefix, Postfix)" << endl << endl;
+    cin.get(method, 7);
+    cin.clear();
+    cin.ignore(1000000, '\n');
+    if (strcmp(method, "Infix") == 0 || strcmp(method, "infix") == 0) {
+      cout << endl << "Infix" << endl;
+      break;
+    }
+    else if (strcmp(method, "Prefix") == 0 || strcmp(method, "prefix") == 0) {
+      cout << endl << "Prefix" << endl;
+      break;
+    }
+    else if (strcmp(method, "Postfix") == 0 || strcmp(method, "postfix") == 0) {
+      cout << endl << "Postfix" << endl;
+      break;
+    }
+    else {
+      cout << endl << "Invalid Input" << endl;
+    }
+  }
+  
+  return 0;
+}
