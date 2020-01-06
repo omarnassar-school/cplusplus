204ff6df294c2c317fc7e6d72f84828a0bff9091
U-BSD\377576 <377576@SHS-10DEC49DF.beaverton.k12.or.us>
2020-01-06 08:15:19 -0800

    first commit

--- LinkedList/Part2/Node.h
+++ LinkedList/Part2/Node.h
@@ -1,23 +1,25 @@
-#ifndef node_h
-#define node_h
-
+//define
+#ifndef NODE_H
+#define NODE_H
+//imports
 #include <iostream>
 #include <cstring>
 #include "Student.h"
 
 using namespace std;
 
-class Node {
+class Node { //class for nodes in the list
  public:
-  Node(Student*);
-  ~Node();
-  Student* getStudent();
-  void setStudent(Student*);
-  Node* getNext();
-  void setNext(Node*);
+  Node(Student*); //constructor for node with pointer to student class
+  ~Node(); //destructor
+  //getters and setters
+  void setStudent(Student* newstudent); //set the student
+  Student* getStudent(); //return student pointer
+  void setNext(Node* newnext); //set next node
+  Node* getNext(); //returns next node
  private:
-  Student* student;
-  Node* next;
+  Student* student; //student held in node
+  Node* next; //next node in list
 };
 
 #endif
