a23439e92e0a9abea575d9d37e189a6c8185a03d
U-BSD\377576 <377576@SHS-10DEC49DF.beaverton.k12.or.us>
2020-01-10 08:53:25 -0800

    first commit

--- ShuntingYard/main.cpp
+++ ShuntingYard/main.cpp
@@ -6,17 +6,22 @@
 
 #include <iostream>
 #include <cstring>
-#include <vector>
+#include <ctype.h>
+#include <stack>
+#include <queue>
 #include "node.h"
 
 
 using namespace std;
 
 void parseInput(vector<char*> &parsedInput, char input[100]);
+void printVector(vector<char*>);
+void intopost(vector<char*>& parsedInput, queue<char*>& Queue);
 
 int main() {
   char input[100];
   vector<char*> parsedInput;
+  queue<char*> Queue;
   cout << "Enter an infix expression" << endl << endl;
   cin.get(input, 100);
   cin.clear();
@@ -49,12 +54,45 @@ int main() {
 }
 
 void parseInput(vector<char*> &parsedInput, char input[100]) {
-  cout << "works" << endl;
+  //cout << "works" << endl;
   int pointers[2];
   pointers[0] = -1;
-  char* newArray = new char[strlen(input)];
   for (int i = 0; i < strlen(input); i++) {
+    if (input[i] == ' ') {
+      pointers[1] = i;
+      int j = 0;
+      char* newArray = new char[pointers[1] - pointers[0]];
+      for (int i = pointers[0] + 1; i < pointers[1]; i++) {
+	newArray[j] = input[i];
+	j = j + 1;
+      }
+      newArray[j] = '\0';
+      parsedInput.push_back(newArray);
+      pointers[0] = pointers[1];
+    }
+  }
+  char* newArray = new char[strlen(input) - pointers[0]];
+  int j = 0;
+  for (int i = pointers[0] + 1; i < strlen(input); i++){
+    newArray[j] = input[i];
+    j = j + 1;
+  }
+  newArray[j] = '\0';
+  parsedInput.push_back(newArray);
+  printVector(parsedInput);
+}
+
+void printVector(vector<char*> parsedInput) {
+  for (int i = 0; i < parsedInput.size(); i ++) {
+    cout << parsedInput[i];
+  }
+  cout << endl;
+}
+
+void intopost(vector<char*>& parsedInput, queue<char*>& Queue) {
+  stack<char*> Stack;
+  int i = 0;
+  while (i < parsedInput.size()) {
     
   }
-  parsedInput.push_back(newArray);
 }
