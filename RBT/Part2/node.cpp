4c78146819872c81b3d0c8237a309c9a386be793
omarnassar-school <54950372+omarnassar-school@users.noreply.github.com>
2020-03-31 11:25:25 -0700

    starting Red-Black Tree deletion

---
+++ RBT/Part2/node.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "node.h"
+
+using namespace std;
+
+Node::Node() {
+  parent = NULL;
+  left = NULL;
+  right = NULL;
+  sibling = NULL;
+  color = true;
+}
+
+Node::~Node() {
+  delete &value;
+  parent = NULL;
+  left = NULL;
+  right = NULL;
+  sibling = NULL;
+}
+
+//Getters
+
+Node* Node::getParent() {
+  return parent;
+}
+
+Node* Node::getLeft() {
+  return left;
+}
+
+Node* Node::getRight() {
+  return right;
+}
+
+int Node::getValue() {
+  return value;
+}
+
+Node* Node::getSibling() {
+  return sibling;
+}
+
+bool Node::getColor() {
+  return color;
+}
+
+//Setters
+
+void Node::setParent(Node* inParent) {
+  parent = inParent;
+}
+
+void Node::setLeft(Node* inLeft) {
+  left = inLeft;
+}
+
+void Node::setRight(Node* inRight) {
+  right = inRight;
+}
+
+void Node::setValue(int inValue) {
+  value = inValue;
+}
+
+void Node::setSibling(Node* inSibling) {
+  sibling = inSibling;
+}
+
+void Node::setColor(bool inColor) {
+  color = inColor;
+}
