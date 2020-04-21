28d0761ba33deefbcbe986b3dc8fc7b636206736
omarnassar-school <54950372+omarnassar-school@users.noreply.github.com>
2020-04-21 11:28:26 -0700

    working on Graph Creator

--- GraphCreator/main.cpp
+++ GraphCreator/main.cpp
@@ -29,10 +29,65 @@ void findShortest(Vertex* first, Vertex* second) {
 }
 
 void printAdjacency() {
-
+  
 }
 
 int main() {
   
+  int numInput;
+  char charInput;
+  
+  while (true) {
+    cout << endl << "Would you like to:" << endl;
+    cout << "1: Add a vertex" << endl;
+    cout << "2: Add an edge" << endl;
+    cout << "3: Remove a vertex" << endl;
+    cout << "4: Remove an edge" << endl;
+    cout << "5: Find the shortest path between two points" << endl;
+    cout << "6: Print the adjacency matrix" << endl;
+    cout << "7: Quit" << endl;
+    cout << endl << ">> ";
+    
+    cin >> numInput;
+    cin.clear();
+    cin.ignore(1000000, '\n');
+    
+    if (numInput == 1) {//add vertex
+      cout << "What character will you assign the vertex?" << endl << ">> ";
+      cin >> charInput;
+      cin.clear();
+      cin.ignore(1000000, '\n');
+      addVertex(charInput);
+    }
+
+    else if (numInput == 2) {//add edge
+
+    }
+
+    else if (numInput == 3) {//remove vertex
+
+    }
+
+    else if (numInput == 4) {//remove edge
+
+    }
+
+    else if (numInput == 5) {//shortest path
+
+    }
+
+    else if (numInput == 6) {//print
+
+    }
+
+    else if (numInput == 7) {//quit
+      break;
+    }
+
+    else {
+      cout << "Invalid Input." << endl;
+      break;
+    }
+  }
   return 0;
 }
