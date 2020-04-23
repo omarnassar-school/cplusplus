f45d294769e60910fcad6572d929b1b179d85b4e
omarnassar-school <54950372+omarnassar-school@users.noreply.github.com>
2020-04-23 11:53:14 -0700

    can add vertices, edges, and print matrix

--- GraphCreator/main.cpp
+++ GraphCreator/main.cpp
@@ -19,7 +19,20 @@ vector<Edge*> edges;
 
 void addVertex(char label) {
   Vertex* temp = new Vertex(label);
-  vertices.push_back(temp);
+  bool exists = false;
+  vector<Vertex*>::iterator i;
+  for (i = vertices.begin(); i != vertices.end(); i++) {
+    if ((*i) -> getLabel() == label)
+      exists = true;
+  }
+  
+  if (!exists) {
+    vertices.push_back(temp);
+    cout << endl << "Vertex has been added." << endl;
+  }
+  else
+    cout << endl << "Cannot have duplicate labels." << endl;
+  
   temp = NULL;
   delete temp;
 }
@@ -28,26 +41,102 @@ void removeVertex() {
   
 }
 
-void addEdge(Vertex* first, Vertex* second, int weight) {
+void addEdge(char firstLabel, char secondLabel, int weight) {
+  Edge* temp = new Edge();
+  Vertex* first = NULL;
+  Vertex* second = NULL;
+  bool exists = false;
   
+  vector<Vertex*>::iterator i;
+  for (i = vertices.begin(); i != vertices.end(); i++) {
+    if ((*i) -> getLabel() == firstLabel) {
+      first = *i;
+    }
+    else if ((*i) -> getLabel() == secondLabel) {
+      second = *i;
+    }
+  }
+  
+  vector<Edge*>::iterator e;
+  for (e = edges.begin(); e != edges.end(); e++) {
+    if (((*e) -> getFirst() == first && (*e) -> getSecond() == second))
+      exists = true;
+  }
+  
+  if (first != NULL && second != NULL && !exists) {
+    temp -> setFirst(first);
+    temp -> setSecond(second);
+    temp -> setWeight(weight);
+    edges.push_back(temp);
+    cout << endl << "Edge has been added." << endl;
+  }
+  else if (exists)
+    cout << endl << "There is already an edge connecting these two vertices." << endl;
+  else
+    cout << endl << "One of the vertices does not exist." << endl;
+
+  first = NULL;
+  second = NULL;
+  temp = NULL;
+  delete first;
+  delete second;
+  delete temp;
 }
 
-void removeEdge() {
+void removeEdge(char firstLabel, char secondLabel) {
 
 }
 
-void findShortest(Vertex* first, Vertex* second) {
+void findShortest(char firstLabel, char  secondLabel) {
   
 }
 
 void printAdjacency() {
+  if (vertices.empty())
+    return;
   
+  cout << "\t";
+  vector<Vertex*>::iterator v;
+  for (v = vertices.begin(); v != vertices.end(); v++) {
+    cout << (*v) -> getLabel() << "\t";
+  }
+  cout << endl;
+
+  vector<Vertex*>::iterator v2;
+  vector<Edge*>::iterator e;
+  for (v = vertices.begin(); v != vertices.end(); v++) {
+    cout << (*v) -> getLabel() << "\t";
+    for (v2 = vertices.begin(); v2 != vertices.end(); v2++) {
+      bool connection = false;
+      if (v2 == v) {
+	cout << "n/a";
+	connection = true;
+      }
+      else {
+	for (e = edges.begin(); e != edges.end(); e++) {
+	  if ((*e) -> getFirst() -> getLabel() == (*v) -> getLabel()) {
+	    if ((*e) -> getSecond() -> getLabel() == (*v2) -> getLabel() ) {
+	      cout << (*e) -> getWeight();
+	      connection = true;
+	    }
+	  }
+	}
+      }
+      
+      if (!connection)
+	cout << "none";
+      
+      cout << "\t";
+    }
+    cout << endl;
+  }
 }
 
 int main() {
   
   int numInput;
   char charInput;
+  char charInput2;
   
   while (true) {
     cout << endl << "Would you like to:" << endl;
@@ -73,11 +162,23 @@ int main() {
     }
 
     else if (numInput == 2) {//add edge
-
+      cout << endl << "What is the label of the first vertex?" << endl << ">> ";
+      cin >> charInput;
+      cin.clear();
+      cin.ignore(1000000, '\n');
+      cout << endl << "What is the label of the second vertex?" << endl << ">> ";
+      cin >> charInput2;
+      cin.clear();
+      cin.ignore(1000000, '\n');
+      cout << endl << "What is the weight of this edge?" << endl << ">> ";
+      cin >> numInput;
+      cin.clear();
+      cin.ignore(1000000, '\n');
+      addEdge(charInput, charInput2, numInput);
     }
 
     else if (numInput == 3) {//remove vertex
-
+      
     }
 
     else if (numInput == 4) {//remove edge
@@ -88,18 +189,16 @@ int main() {
 
     }
 
-    else if (numInput == 6) {//print
-
-    }
+    else if (numInput == 6) //print
+      printAdjacency();
 
     else if (numInput == 7) {//quit
       break;
     }
 
-    else {
+    else
       cout << "Invalid Input." << endl;
-      break;
-    }
+    
   }
   return 0;
 }
